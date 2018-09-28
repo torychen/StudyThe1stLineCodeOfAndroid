9695f1a00ff01ffeda64789f84d885e02c504518
admin <3903927@qq.com>
2018-09-28 16:49:05 +0800

    support toast by button 1

--- Code/CH01/ActivityTest/app/src/main/java/com/tc/activitytest/FirstActivity.java
+++ Code/CH01/ActivityTest/app/src/main/java/com/tc/activitytest/FirstActivity.java
@@ -2,6 +2,9 @@ package com.tc.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        Button btn1 = findViewById(R.id.btn_for_toast);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
