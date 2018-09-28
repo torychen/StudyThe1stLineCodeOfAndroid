6ae18cfc62c5b4fb574b6eeb9fb864ba33828e7a
admin <3903927@qq.com>
2018-09-28 15:42:27 +0800

    try logd, logi shortcuts.

--- Code/CH01/HelloWorld/app/src/main/java/com/tc/helloworld/HelloWorldActivity.java
+++ Code/CH01/HelloWorld/app/src/main/java/com/tc/helloworld/HelloWorldActivity.java
@@ -2,12 +2,17 @@ package com.tc.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelloWorldActivity extends AppCompatActivity {
-
+    private static final String TAG = "HelloWorldActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        Log.d("HelloWorldActivity", "onCreate execute");
+        Log.i(TAG, "onCreate: this is log.i().");
+        Log.d("data", "onCreate: by tag data");
     }
 }
