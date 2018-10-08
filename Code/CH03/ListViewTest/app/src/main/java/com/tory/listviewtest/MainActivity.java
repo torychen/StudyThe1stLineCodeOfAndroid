2ebd72a6d76508ed0abbc52785daf62f735f41f4
admin <3903927@qq.com>
2018-10-08 16:45:10 +0800

    Pass UT.

---
+++ Code/CH03/ListViewTest/app/src/main/java/com/tory/listviewtest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.tory.listviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  String[] data = {
+            "Apple",
+            "Banana",
+            "Orange",
+            "Watermelon",
+            "Pear",
+            "Grape",
+            "Pineapple","Strawberry","Cherry","Mango" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView lv = findViewById(R.id.list_view);
+        lv.setAdapter(adapter);
+    }
+}
