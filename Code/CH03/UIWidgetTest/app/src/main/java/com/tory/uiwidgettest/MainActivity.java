9a9456225880d6b2c0ba559d9cf1b7e1c8482bb0
admin <3903927@qq.com>
2018-10-08 15:22:47 +0800

    Support up to AlertDialog

--- Code/CH03/UIWidgetTest/app/src/main/java/com/tory/uiwidgettest/MainActivity.java
+++ Code/CH03/UIWidgetTest/app/src/main/java/com/tory/uiwidgettest/MainActivity.java
@@ -1,10 +1,14 @@
 package com.tory.uiwidgettest;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,13 +18,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ImageView imageView = findViewById(R.id.image_view);
         final EditText editText = findViewById(R.id.edit_text);
+        final ProgressBar progressBar = findViewById(R.id.progress_bar);
+
+
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setTitle("This is an AlertDialog");
+                ad.setMessage("Something important");
+                ad.setCancelable(false);
+                ad.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //do nothing.
+                    }
+                });
+
+                ad.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //do nothing.
+                    }
+                });
+
+                ad.show();
+
+
                 String str = editText.getText().toString();
                 Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+
+                imageView.setImageResource(R.drawable.banana_pic);
+
+                if (View.GONE == progressBar.getVisibility()) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(10 + progressBar.getProgress());
+
+                }
+                else {
+                    progressBar.setVisibility(View.GONE);
+                }
             }
         });
 
