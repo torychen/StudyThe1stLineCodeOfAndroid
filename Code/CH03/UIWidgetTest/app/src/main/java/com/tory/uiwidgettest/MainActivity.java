db1331cf68fe870a1788677d35efcaea4e4d77f0
admin <3903927@qq.com>
2018-10-08 14:44:15 +0800

    Support up to EditView

--- Code/CH03/UIWidgetTest/app/src/main/java/com/tory/uiwidgettest/MainActivity.java
+++ Code/CH03/UIWidgetTest/app/src/main/java/com/tory/uiwidgettest/MainActivity.java
@@ -2,6 +2,10 @@ package com.tory.uiwidgettest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.edit_text);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = editText.getText().toString();
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
