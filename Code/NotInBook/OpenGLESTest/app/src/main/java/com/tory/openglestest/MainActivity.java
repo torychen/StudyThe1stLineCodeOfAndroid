15c81501c7580f0782e40b0b3c76180b67bcfcc4
admin <3903927@qq.com>
2018-10-16 10:15:05 +0800

    Pass build.

--- Code/NotInBook/OpenGLESTest/app/src/main/java/com/tory/openglestest/MainActivity.java
+++ Code/NotInBook/OpenGLESTest/app/src/main/java/com/tory/openglestest/MainActivity.java
@@ -1,13 +1,35 @@
 package com.tory.openglestest;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GLSurfaceView mGlSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGlSurfaceView = findViewById(R.id.glsvMain);
+        mGlSurfaceView.setEGLContextClientVersion(2);
+        mGlSurfaceView.setEGLConfigChooser(8,8,8,8,16,0);//tory ?
+        mGlSurfaceView.setRenderer(new MyRenderer());
+        mGlSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGlSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGlSurfaceView.onResume();
     }
 }
