6df35dd2155da2c0f7060be43976250aa5683127
admin <3903927@qq.com>
2018-11-05 14:59:50 +0800

    tmp ci ch03

---
+++ StudyProgress/第三章常用控件.mm
@@ -0,0 +1,47 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1541399843889" ID="Freemind_Link_807788290" MODIFIED="1541400189394" TEXT="&#x7b2c;&#x4e09;&#x7ae0;&#x5e38;&#x7528;&#x63a7;&#x4ef6;">
+<edge COLOR="#009999" WIDTH="4"/>
+<node COLOR="#000000" CREATED="1541399899265" ID="_" MODIFIED="1541400189394" POSITION="right" TEXT="TextView">
+<edge COLOR="#009999" WIDTH="4"/>
+<node COLOR="#000000" CREATED="1541399992435" ID="Freemind_Link_654458114" MODIFIED="1541400189394" TEXT="android:gravity &#x5bf9;&#x9f50;&#x65b9;&#x5f0f;">
+<edge COLOR="#009999" WIDTH="4"/>
+</node>
+<node COLOR="#000000" CREATED="1541400021501" ID="Freemind_Link_25796858" MODIFIED="1541400189394" TEXT="android:textSize">
+<edge COLOR="#009999" WIDTH="4"/>
+</node>
+<node COLOR="#000000" CREATED="1541400031596" ID="Freemind_Link_1781874291" MODIFIED="1541400189393" TEXT="android:textColor">
+<edge COLOR="#009999" WIDTH="4"/>
+</node>
+</node>
+<node COLOR="#000000" CREATED="1541400055425" HGAP="23" ID="Freemind_Link_1822004871" MODIFIED="1541400189393" POSITION="right" TEXT="Button" VSHIFT="22">
+<edge COLOR="#009999" WIDTH="4"/>
+<node COLOR="#000000" CREATED="1541400069200" ID="Freemind_Link_70943377" MODIFIED="1541400189393" TEXT="android&#xff1a;textAllCaps">
+<edge COLOR="#009999" WIDTH="4"/>
+</node>
+<node COLOR="#000000" CREATED="1541400115139" ID="Freemind_Link_294700191" MODIFIED="1541400189393" TEXT="setOnClickListener(View.onClickListener)">
+<edge COLOR="#009999" WIDTH="4"/>
+</node>
+</node>
+<node CREATED="1541400245503" ID="Freemind_Link_172820123" MODIFIED="1541400253400" POSITION="right" TEXT="EditText" VSHIFT="39">
+<node CREATED="1541400255555" ID="Freemind_Link_134987337" MODIFIED="1541400269069" TEXT="android:hint=&quot;&#x63d0;&#x793a;&#x4fe1;&#x606f;&quot;"/>
+<node CREATED="1541400311704" ID="Freemind_Link_1884139129" MODIFIED="1541400332877" TEXT="android&#xff1a;maxLines &#x6700;&#x5927;&#x884c;&#x6570;"/>
+<node CREATED="1541400333405" ID="Freemind_Link_1205063348" MODIFIED="1541400367531" TEXT="getText().toString() &#x83b7;&#x53d6;&#x7528;&#x6237;&#x8f93;&#x5165;&#x7684;&#x5b57;&#x7b26;"/>
+</node>
+<node CREATED="1541400400509" HGAP="27" ID="Freemind_Link_862541245" MODIFIED="1541400407521" POSITION="right" TEXT="ImageView" VSHIFT="48">
+<node CREATED="1541400414197" ID="Freemind_Link_453389087" MODIFIED="1541400437125" TEXT="res/ drawable&#xff0c; drawable-xhdpi &#x76ee;&#x5f55;"/>
+<node CREATED="1541400493923" ID="Freemind_Link_930475456" MODIFIED="1541400503367" TEXT="setImageRsource()"/>
+</node>
+<node CREATED="1541400529565" HGAP="26" ID="Freemind_Link_905467037" MODIFIED="1541400536510" POSITION="right" TEXT="ProgressBar" VSHIFT="63">
+<node CREATED="1541400689934" ID="Freemind_Link_319820784" MODIFIED="1541400757062" TEXT="&#x6c34;&#x5e73;&#x8fdb;&#x5ea6;&#x6761;&#xa;style=&quot;?android:attr/progressBarStyleHorizontal&quot;&#xa;android:max=&quot;100&quot;&#xa;&#xa;setProgress()"/>
+</node>
+<node CREATED="1541400771000" HGAP="28" ID="Freemind_Link_255641267" MODIFIED="1541400777695" POSITION="right" TEXT="AlertDialog" VSHIFT="57">
+<node CREATED="1541400781746" ID="Freemind_Link_1085602324" MODIFIED="1541401009229" TEXT="&#x5f39;&#x51fa;&#x5bf9;&#x8bdd;&#x6846;&#xff0c;&#x5c4f;&#x853d;&#x5176;&#x5b83;&#x63a7;&#x4ef6;&#x7684;&#x4ea4;&#x4e92;&#x80fd;&#x529b;&#xa;&#x7528; builder &#x521b;&#x5efa;&#xff0c;&#xa;AletDialog.Builder dialog = new XXX;&#xa;&#xa;&#x8bbe;&#x7f6e;&#x6309;&#x952e;&#x7684;&#x54cd;&#x5e94;&#x51fd;&#x6570;&#xa;dialog.setPositivButton(&quot;OK&quot;, new DislogInterface.OnClickListener(){})&#xa;&#xa;dialog.show()"/>
+</node>
+<node CREATED="1541401017058" HGAP="24" ID="Freemind_Link_1705713583" MODIFIED="1541401044415" POSITION="right" TEXT="ProgressDialog&#xff1a; &#x663e;&#x793a;&#x5bf9;&#x8bdd;&#x6846;&#x7684;&#x540c;&#x65f6;&#xff0c;&#x6709;&#x4e00;&#x4e2a;&#x65cb;&#x8f6c;&#x7684;&#x8fdb;&#x5ea6;&#x63d0;&#x793a;" VSHIFT="47"/>
+<node CREATED="1541400549619" ID="Freemind_Link_14070087" MODIFIED="1541400654982" POSITION="left" TEXT="&#x63a7;&#x4ef6;&#x7684;&#x53ef;&#x89c1;&#x6027;&#xff1a; android:visibility,  setVisibility()&#xa;visible&#xff0c; &#x53ef;&#x89c1;&#xa;invisible&#xff0c; &#x4e0d;&#x53ef;&#x89c1;&#xff0c;&#x900f;&#x660e;&#xff0c;&#x4f46;&#x662f;&#x5360;&#x636e;&#x5c4f;&#x5e55;&#xa;gone&#xff0c;&#x4e0d;&#x53ef;&#x89c1;&#xff0c;&#x540c;&#x65f6;&#x4e0d;&#x518d;&#x5360;&#x636e;&#x4efb;&#x4f55;&#x5c4f;&#x5e55;&#x7a7a;&#x95f4;&#xa;"/>
+<node CREATED="1541401073400" HGAP="30" ID="Freemind_Link_389520783" MODIFIED="1541401107295" POSITION="left" TEXT="&#x5e03;&#x5c40;&#xff0c;&#x53ef;&#x4ee5;&#x653e;&#x7f6e;&#x63a7;&#x4ef6;&#x548c;&#x6216;&#x5e03;&#x5c40;" VSHIFT="97">
+<node CREATED="1541401135735" ID="Freemind_Link_311892528" MODIFIED="1541401174174" TEXT="LinearLayout &#x7ebf;&#x6027;&#x5e03;&#x5c40;&#xa;anroid&#xff1a;oritentation &#x5782;&#x76f4;&#x6216;&#x6c34;&#x5e73;&#x6392;&#x5217;&#x63a7;&#x4ef6;&#x3002;"/>
+</node>
+</node>
+</map>
