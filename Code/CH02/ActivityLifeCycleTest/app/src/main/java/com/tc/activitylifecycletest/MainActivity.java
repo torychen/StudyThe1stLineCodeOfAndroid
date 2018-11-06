79c816c0a1f0c15d8cda8faa58d04bcd15a75e75
admin <3903927@qq.com>
2018-11-06 09:54:02 +0800

    updates activity life cycle

--- Code/CH02/ActivityLifeCycleTest/app/src/main/java/com/tc/activitylifecycletest/MainActivity.java
+++ Code/CH02/ActivityLifeCycleTest/app/src/main/java/com/tc/activitylifecycletest/MainActivity.java
@@ -2,6 +2,7 @@ package com.tc.activitylifecycletest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,45 +14,75 @@ public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Toast.makeText(MainActivity.this, "onConfigurationChanged()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onConfigurationChanged: ");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(MainActivity.this, "onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onRestoreInstanceState: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(MainActivity.this, "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onSaveInstanceState: ");
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(MainActivity.this, "onStart()", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(MainActivity.this, "onResume()", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(MainActivity.this, "onPause()", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(MainActivity.this, "onStop()", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(MainActivity.this, "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStart: ");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Toast.makeText(MainActivity.this, "onRestart()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onRestart: ");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(MainActivity.this, "onResume()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this, "onPause()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this, "onStop()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this, "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: ");
 
 
 
