95bf66ce2ebe1020cd694105ee71ec8f0898f147
admin <3903927@qq.com>
2018-11-07 17:02:38 +0800

    ch09 updates

---
+++ StudyProgress/第九章网络技术.mm
@@ -0,0 +1,10 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1541581109778" ID="Freemind_Link_1657022671" MODIFIED="1541581174083" TEXT="&#x7b2c;&#x4e5d;&#x7ae0;&#x7f51;&#x7edc;&#x6280;&#x672f;">
+<edge COLOR="#009999" WIDTH="4"/>
+<node COLOR="#000000" CREATED="1541581152018" ID="_" MODIFIED="1541581188306" POSITION="right" TEXT="WebView &#x63a7;&#x4ef6; &#x5728;&#x81ea;&#x5df1;&#x7684;&#x5e94;&#x7528;&#x4e2d;&#x5d4c;&#x5165;&#x6d4f;&#x89c8;&#x5668;">
+<edge COLOR="#009999" WIDTH="4"/>
+<node CREATED="1541581255382" ID="Freemind_Link_1289206306" MODIFIED="1541581341960" TEXT="WebView webView = (WebView) findViewById(R.id.web_view);&#xa;webView.getSettings().setJavaScriptEnabled(true);&#xa;&#xa;//&#x4fdd;&#x8bc1;&#x7f51;&#x9875;&#x5728;&#x5f53;&#x524d;webview&#x663e;&#x793a;&#xff0c;&#x800c;&#x4e0d;&#x662f;&#x6253;&#x5f00;&#x7cfb;&#x7edf;&#x6d4f;&#x89c8;&#x5668;&#xa;webView.setWebViewClient(new WebViewClient());&#xa;&#xa;webView.loadUrl(&quot;https://www.baidu.com&quot;);"/>
+</node>
+</node>
+</map>
