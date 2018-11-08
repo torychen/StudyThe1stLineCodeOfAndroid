e24f38a77df4fc453775acb7ddc04f55a23cdce1
admin <3903927@qq.com>
2018-11-08 14:33:43 +0800

    add material test

--- StudyProgress/第十二章MaterialDesign.mm
+++ StudyProgress/第十二章MaterialDesign.mm
@@ -10,22 +10,38 @@
 <node CREATED="1541656167585" ID="Freemind_Link_1052433308" MODIFIED="1541656172231" TEXT="&lt;android.support.v7.widget.Toolbar&#xa;    android:id=&quot;@+id/toolbar&quot;&#xa;    android:layout_width=&quot;match_parent&quot;&#xa;    android:layout_height=&quot;?attr/actionBarSize&quot;&#xa;    android:background=&quot;?attr/colorPrimary&quot;&#xa;    android:theme=&quot;@style/ThemeOverlay.AppCompat.Dark.ActionBar&quot;&#xa;    app:popupTheme=&quot;@style/ThemeOverlay.AppCompat.Light&quot;&#xa;    app:layout_scrollFlags=&quot;scroll|enterAlways|snap&quot; /&gt;"/>
 </node>
 <node CREATED="1541656317768" ID="Freemind_Link_646209846" MODIFIED="1541656324633" POSITION="right" TEXT="&#x6ed1;&#x52a8;&#x83dc;&#x5355;" VSHIFT="79">
-<node CREATED="1541656326164" ID="Freemind_Link_692965439" MODIFIED="1541656332718" TEXT="DrawerLayout"/>
+<node CREATED="1541656326164" ID="Freemind_Link_692965439" MODIFIED="1541656332718" TEXT="DrawerLayout">
+<node CREATED="1541658373465" ID="Freemind_Link_97268358" MODIFIED="1541658375059" TEXT="&lt;android.support.v4.widget.DrawerLayout"/>
+</node>
 <node CREATED="1541656520687" HGAP="23" ID="Freemind_Link_1441117050" MODIFIED="1541656537826" TEXT="NavigationView" VSHIFT="63">
-<node CREATED="1541656637524" ID="Freemind_Link_342365249" MODIFIED="1541656652074" TEXT="&#x9700;&#x8981; menu &#x548c; headerLayout"/>
+<node CREATED="1541656637524" ID="Freemind_Link_342365249" MODIFIED="1541656652074" TEXT="&#x9700;&#x8981; menu &#x548c; headerLayout">
+<node CREATED="1541658494353" ID="Freemind_Link_258897699" MODIFIED="1541658495233" TEXT="&lt;android.support.design.widget.NavigationView"/>
+</node>
 </node>
 </node>
 <node CREATED="1541656678567" HGAP="23" ID="Freemind_Link_52160074" MODIFIED="1541656692209" POSITION="right" TEXT="&#x60ac;&#x6d6e;&#x6309;&#x94ae;&#x548c;&#x53ef;&#x4ea4;&#x4e92;&#x63d0;&#x793a;" VSHIFT="43">
-<node CREATED="1541656751457" ID="Freemind_Link_1639396862" MODIFIED="1541656760191" TEXT="FloatingActionButton"/>
+<node CREATED="1541656751457" ID="Freemind_Link_1639396862" MODIFIED="1541656760191" TEXT="FloatingActionButton">
+<node CREATED="1541658472941" ID="Freemind_Link_762370291" MODIFIED="1541658473999" TEXT=" &lt;android.support.design.widget.FloatingActionButton"/>
+</node>
 <node CREATED="1541656842526" ID="Freemind_Link_1347738809" MODIFIED="1541656892158" TEXT="Snackbar&#xa;Toast&#x53ea;&#x662f;&#x901a;&#x77e5;&#xff0c;&#x800c;Snackbar &#x53ef;&#x4ee5;&#x901a;&#x77e5;&#x7684;&#x540c;&#x65f6;&#x548c;&#x7528;&#x6237;&#x4ea4;&#x4e92;" VSHIFT="61"/>
 </node>
-<node CREATED="1541657114779" ID="Freemind_Link_908324254" MODIFIED="1541657217679" POSITION="right" TEXT="CoordingnatorLayout &#x662f;&#x4e00;&#x4e2a;&#x52a0;&#x5f3a;&#x7248;&#x7684; FrameLayout&#xa;&#x53ef;&#x4ee5;&#x76d1;&#x542c;&#x6240;&#x6709;&#x5b50;&#x63a7;&#x4ef6;&#x7684;&#x4e8b;&#x4ef6;&#xff0c;&#x81ea;&#x52a8;&#x5e2e;&#x6211;&#x4eec;&#x505a;&#x51fa;&#x5408;&#x7406;&#x7684;&#x54cd;&#x5e94;&#x3002;" VSHIFT="31"/>
-<node CREATED="1541657268498" ID="Freemind_Link_786234583" MODIFIED="1541657301097" POSITION="right" TEXT="CardView &#x5b9e;&#x9645;&#x4e0a;&#x4e5f;&#x662f;&#x4e00;&#x4e2a; FrameLayout" VSHIFT="68"/>
-<node CREATED="1541657587606" ID="Freemind_Link_601607843" MODIFIED="1541657595585" POSITION="right" TEXT="AppBarLayout" VSHIFT="28"/>
-<node CREATED="1541657747293" HGAP="21" ID="Freemind_Link_620562152" MODIFIED="1541657765881" POSITION="right" TEXT="&#x4e0b;&#x62c9;&#x5237;&#x65b0; SwipRefreshLayout" VSHIFT="30"/>
-<node CREATED="1541657875770" ID="Freemind_Link_1474012885" MODIFIED="1541657884417" POSITION="right" TEXT="&#x53ef;&#x6298;&#x53e0;&#x5f0f;&#x6807;&#x9898;&#x680f;" VSHIFT="50"/>
+<node CREATED="1541657114779" ID="Freemind_Link_908324254" MODIFIED="1541657217679" POSITION="right" TEXT="CoordingnatorLayout &#x662f;&#x4e00;&#x4e2a;&#x52a0;&#x5f3a;&#x7248;&#x7684; FrameLayout&#xa;&#x53ef;&#x4ee5;&#x76d1;&#x542c;&#x6240;&#x6709;&#x5b50;&#x63a7;&#x4ef6;&#x7684;&#x4e8b;&#x4ef6;&#xff0c;&#x81ea;&#x52a8;&#x5e2e;&#x6211;&#x4eec;&#x505a;&#x51fa;&#x5408;&#x7406;&#x7684;&#x54cd;&#x5e94;&#x3002;" VSHIFT="31">
+<node CREATED="1541658403922" ID="Freemind_Link_1681395357" MODIFIED="1541658405196" TEXT="&lt;android.support.design.widget.CoordinatorLayout"/>
+</node>
+<node CREATED="1541657268498" ID="Freemind_Link_786234583" MODIFIED="1541657301097" POSITION="right" TEXT="CardView &#x5b9e;&#x9645;&#x4e0a;&#x4e5f;&#x662f;&#x4e00;&#x4e2a; FrameLayout" VSHIFT="68">
+<node CREATED="1541658696405" ID="Freemind_Link_1772720083" MODIFIED="1541658698076" TEXT="&lt;android.support.v7.widget.CardView"/>
+</node>
+<node CREATED="1541657587606" ID="Freemind_Link_601607843" MODIFIED="1541657595585" POSITION="right" TEXT="AppBarLayout" VSHIFT="28">
+<node CREATED="1541658419943" ID="Freemind_Link_165365067" MODIFIED="1541658421034" TEXT="&lt;android.support.design.widget.AppBarLayout"/>
+</node>
+<node CREATED="1541657747293" HGAP="21" ID="Freemind_Link_620562152" MODIFIED="1541657765881" POSITION="right" TEXT="&#x4e0b;&#x62c9;&#x5237;&#x65b0; SwipRefreshLayout" VSHIFT="30">
+<node CREATED="1541658454034" ID="Freemind_Link_1907757895" MODIFIED="1541658456378" TEXT="&lt;android.support.v4.widget.SwipeRefreshLayout"/>
+</node>
+<node CREATED="1541657875770" ID="Freemind_Link_1474012885" MODIFIED="1541657963880" POSITION="right" TEXT="&#x53ef;&#x6298;&#x53e0;&#x5f0f;&#x6807;&#x9898;&#x680f; CollapsingToolbarLayout&#xa;&#xa;CollapsingToolbarLayout &#x5fc5;&#x987b;&#x662f;AppBarLayout&#x7684;&#x5b50;&#x5e03;&#x5c40;&#xa;&#xa;AppBarLayout &#x5fc5;&#x987b;&#x662f; CoordinatoryLayout &#x7684;&#x5b50;&#x5e03;&#x5c40;&#xa;&#xa;" VSHIFT="50"/>
+<node CREATED="1541658072332" ID="Freemind_Link_1972560042" MODIFIED="1541658280673" POSITION="right" TEXT="NestedScrollView &#x6bd4; ScrollView &#x589e;&#x52a0;&#x4e86;&#x5d4c;&#x5957;&#x54cd;&#x5e94;&#x6eda;&#x52a8;&#x4e8b;&#x4ef6;&#x7684;&#x529f;&#x80fd;" VSHIFT="31"/>
+<node CREATED="1541658281859" ID="Freemind_Link_1144025614" MODIFIED="1541658315641" POSITION="right" TEXT="&#x5229;&#x7528;&#x7cfb;&#x7edf;&#x72b6;&#x6001;&#x680f;&#x7a7a;&#x95f4; android:fitsSystemWindows" VSHIFT="44"/>
 <node CREATED="1541656693820" ID="Freemind_Link_1075580565" MODIFIED="1541656722879" POSITION="left" TEXT="&#x7acb;&#x9762;&#x8bbe;&#x8ba1;&#xff1a;&#x754c;&#x9762;&#x4e0d;&#x662f;&#x4e00;&#x4e2a;&#x5e73;&#x9762;&#xff0c;&#x800c;&#x662f;&#x7acb;&#x4f53;&#x7684;&#x3002; &#x5982;&#x60ac;&#x6d6e;&#x6309;&#x94ae;&#x3002;"/>
 <node CREATED="1541657254704" HGAP="34" ID="Freemind_Link_386585323" MODIFIED="1541657265081" POSITION="left" TEXT="&#x5361;&#x7247;&#x5f0f;&#x5e03;&#x5c40;" VSHIFT="87"/>
-<node CREATED="1541657861596" ID="Freemind_Link_1933105491" MODIFIED="1541657869969" POSITION="left" TEXT="Design Support &#x5e93;" VSHIFT="56"/>
+<node CREATED="1541657861596" ID="Freemind_Link_1933105491" MODIFIED="1541658557239" POSITION="left" TEXT="Design Support &#x5e93;&#xa;support-v4 &#x5e93;&#xa;appcompat-v7 &#x5e93;" VSHIFT="56"/>
 </node>
 </map>
