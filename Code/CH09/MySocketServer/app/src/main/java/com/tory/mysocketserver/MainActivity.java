998765a1f5d28e6833f95350a9c82e442a62919f
admin <3903927@qq.com>
2018-11-14 17:50:49 +0800

    Updates socket but fail

--- Code/CH09/MySocketServer/app/src/main/java/com/tory/mysocketserver/MainActivity.java
+++ Code/CH09/MySocketServer/app/src/main/java/com/tory/mysocketserver/MainActivity.java
@@ -1,4 +1,5 @@
-package com.tory.mysockettest;
+package com.tory.mysocketserver;
+
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,13 +13,15 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -32,9 +35,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     final static String SEND_TCP = "send tcp from Android";
 
     private boolean mQuitUdpRcv = false;
+    private boolean mQuitTcpRcv = false;
 
+    private List<Socket> mList = new ArrayList<Socket>();
+    private ServerSocket server = null;
     private ExecutorService mExecutorService = null;
     private String receiveMsg;
+    private String sendMsg;
+
 
     class UdpListener {
         public void onRecevied(String data)
@@ -43,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private static final String TAG_TCP = "tcp server";
     TextView mTextViewUdp;
     TextView mTextViewTcp;
     @Override
@@ -55,8 +64,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btnQuitRcvUdp).setOnClickListener(this);
 
         findViewById(R.id.btnSendTcp).setOnClickListener(this);
-        findViewById(R.id.btnConnect).setOnClickListener(this);
-        findViewById(R.id.btnClose).setOnClickListener(this);
+        findViewById(R.id.btnRcvTcp).setOnClickListener(this);
+        findViewById(R.id.btnQuitRcvTcp).setOnClickListener(this);
 
         mTextViewTcp = findViewById(R.id.tvTcp);
         mTextViewTcp.setText("Before receive anything from tcp.");
@@ -64,27 +73,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mTextViewUdp =findViewById(R.id.tvUdp);
         mTextViewUdp.setText("Before receive anything from udp.");
 
-        mExecutorService = Executors.newFixedThreadPool(5);
-
-    }
-
-    class SendHiThenClose implements Runnable {
-        @Override
-        public void run() {
-            sendHiThenClose();
-        }
-    }
-
-    private void sendHiThenClose() {
         try {
-            Socket socket = new Socket(PC_LOCAL_HOST, 8888);
-            PrintWriter printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
-                    socket.getOutputStream(), "UTF-8")), true);
 
-            printWriter.println("hello, server");
-            printWriter.close();
-            socket.close();
+            server = new ServerSocket(PORT);
+            mExecutorService = Executors.newCachedThreadPool();
 
+            Log.d(TAG_TCP, "start server");
+            Toast.makeText(MainActivity.this, "start server.", Toast.LENGTH_SHORT).show();
+
+            Socket client = null;
+            while (true) {
+                client = server.accept();
+                mList.add(client);
+                Toast.makeText(MainActivity.this, "accept one socket.", Toast.LENGTH_SHORT).show();
+                mExecutorService.execute(new Service(client));
+            }
 
 
         } catch (Exception e) {
@@ -92,69 +95,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void connect(View view) {
-        mExecutorService.execute(new  connectService());
-    }
-    public void send(View view) {
-        String sendMsg = SEND_TCP.toString();
-        mExecutorService.execute(new  sendService(sendMsg));
-    }
+    class Service implements Runnable {
+        private Socket socket;
+        private BufferedReader in = null;
+        private PrintWriter printWriter = null;
 
-    public void close(View view) {
-        mExecutorService.execute(new sendService("BYE!"));
-    }
-
-    private PrintWriter printWriter;
-    private static final String TAG_TCP = "tcp";
-    private class sendService implements Runnable {
-        private String msg;
-        public sendService(String msg) {
-            this.msg = msg;
+        public Service(Socket socket) {
+            this.socket = socket;
+            try {
+                printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8")), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+                printWriter.println("成功连接服务器" + "（服务器发送）");
+                Log.d(TAG_TCP,"成功连接服务器");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        @Override
-        public void run() {
-            Log.d(TAG_TCP, "sendService: " + msg);
-            printWriter.println(this.msg);
-        }
-    }
-
-    BufferedReader in;
-    private class connectService implements Runnable {
         @Override
         public void run() {
             try {
-                Socket socket = new Socket(LOCAL_HOST, PORT);
-                socket.setSoTimeout(60000);
-                printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8")), true);
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-                receiveMsg();
-            } catch (Exception e) {
-                Log.e(TAG_TCP, ("connectService:" + e.getMessage()));
-            }
-        }
-    }
-
-    private void receiveMsg() {
-        try {
-            while (true) {
-                if ((receiveMsg = in.readLine()) != null) {
-                    Log.d(TAG_TCP, "receiveMsg:" + receiveMsg);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            mTextViewTcp.setText(receiveMsg);
+                while (true) {
+                    if ((receiveMsg = in.readLine()) != null) {
+                        Log.d(TAG_TCP,"receiveMsg:" + receiveMsg);
+                        if (receiveMsg.equals("BYE!")) {
+                            Log.d(TAG_TCP,"客户端请求断开连接");
+                            printWriter.println("服务端断开连接" + "（服务器发送）");
+                            mList.remove(socket);
+                            in.close();
+                            socket.close();
+                            break;
+                        } else {
+                            sendMsg = "我已接收：" + receiveMsg + "（服务器发送）";
+                            printWriter.println(sendMsg);
                         }
-                    });
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            Log.e(TAG_TCP, "receiveMsg: " + e.toString());
-            e.printStackTrace();
         }
     }
-
 
 
 
@@ -173,34 +154,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 }).start();
                 break;
+            case R.id.btnSendTcp:
+                break;
+            case R.id.btnRcvTcp:
+                break;
+
+            case R.id.btnQuitRcvTcp:
+                mQuitTcpRcv = true;
+                break;
 
             case R.id.btnQuitRcvUdp:
                 mQuitUdpRcv = true;
                 break;
-
-            case R.id.btnSendTcp:
-                //send(view);
-                Toast.makeText(MainActivity.this, "Try to send tcp", Toast.LENGTH_SHORT).show();
-                mExecutorService.execute(new SendHiThenClose());
-                break;
-            case R.id.btnConnect:
-                connect(view);
-                break;
-
-            case R.id.btnClose:
-                close(view);
-                break;
-
-
         }
     }
-
-
 
     private void showRecvUdp (final String msg) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
                 mTextViewUdp.setText(msg);
             }
         });
@@ -234,7 +207,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 try {
                     DatagramSocket datagramSocket = new DatagramSocket(PORT);
                     byte [] buf = SEND_UDP.getBytes();
-                    DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName(PC_LOCAL_HOST), PORT);
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName(LOCAL_HOST), PORT);
 
                     datagramSocket.send(packet);
 
