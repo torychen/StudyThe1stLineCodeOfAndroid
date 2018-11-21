6286fdea4c91aba5a328d73bd471a5bef8df18fa
admin <3903927@qq.com>
2018-11-21 17:22:01 +0800

    android updates

--- StudyProgress/12第十二章MaterialDesign.mm
+++ StudyProgress/12第十二章MaterialDesign.mm
@@ -1,6 +1,6 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#000000" CREATED="1541655559466" ID="Freemind_Link_618343932" MODIFIED="1541655593987" TEXT="&#x7b2c;&#x5341;&#x4e8c;&#x7ae0;MaterialDesign">
+<node COLOR="#000000" CREATED="1541655559466" ID="Freemind_Link_618343932" MODIFIED="1542765123189" TEXT="&#x7b2c;&#x5341;&#x4e8c;&#x7ae0;MaterialDesign&#xa;&#x754c;&#x9762;&#x8bbe;&#x8ba1;&#x8bed;&#x8a00;">
 <edge COLOR="#009999" WIDTH="4"/>
 <node CREATED="1541655676934" ID="Freemind_Link_747603435" MODIFIED="1541655697083" POSITION="right" TEXT="ActionBar &#x6807;&#x9898;&#x680f;&#x88ab;Toolbar&#x53d6;&#x4ee3;">
 <node CREATED="1541655752809" ID="Freemind_Link_345042102" MODIFIED="1541655928480" TEXT="&#x6e05;&#x5355;&#x6587;&#x4ef6;&#xa;&lt;application android:them=&quot;@style/AppTheme&quot;&#xa;&#xa;res/values/styles.xml&#xa;&lt;style name=&quot;AppTheme&quot; parent=&quot;Theme.AppCompat.Light.DarkActionBar&quot;&#xa;&#xa;&#x5176;&#x5b83;&#x4e3b;&#x9898;&#xa;&lt;style name=&quot;AppTheme&quot; parent=&quot;Theme.AppCompat.Light.NoActionBar&quot;&#xa;&lt;style name=&quot;AppTheme&quot; parent=&quot;Theme.AppCompat.NoActionBar&quot;"/>
@@ -8,6 +8,9 @@
 <node COLOR="#000000" CREATED="1541655581273" ID="_" MODIFIED="1541655700792" POSITION="right" TEXT="Toolbar" VSHIFT="42">
 <edge COLOR="#009999" WIDTH="4"/>
 <node CREATED="1541656167585" ID="Freemind_Link_1052433308" MODIFIED="1541656172231" TEXT="&lt;android.support.v7.widget.Toolbar&#xa;    android:id=&quot;@+id/toolbar&quot;&#xa;    android:layout_width=&quot;match_parent&quot;&#xa;    android:layout_height=&quot;?attr/actionBarSize&quot;&#xa;    android:background=&quot;?attr/colorPrimary&quot;&#xa;    android:theme=&quot;@style/ThemeOverlay.AppCompat.Dark.ActionBar&quot;&#xa;    app:popupTheme=&quot;@style/ThemeOverlay.AppCompat.Light&quot;&#xa;    app:layout_scrollFlags=&quot;scroll|enterAlways|snap&quot; /&gt;"/>
+<node CREATED="1542768417904" ID="Freemind_Link_477487390" MODIFIED="1542768435575" TEXT="menu &#x83dc;&#x5355;&#x5355;&#x72ec;&#x7684; xml &#x6587;&#x4ef6;">
+<node CREATED="1542768437548" ID="Freemind_Link_1442899472" MODIFIED="1542768459078" TEXT="&lt;menu xmlns:android=&quot;http://schemas.android.com/apk/res/android&quot;&#xa;    xmlns:app=&quot;http://schemas.android.com/apk/res-auto&quot;&#xa;    xmlns:tools=&quot;http://schemas.android.com/tools&quot;&#xa;    tools:context=&quot;com.tory.rednov.MainActivity&quot;&gt;&#xa;    &lt;item&#xa;        android:id=&quot;@+id/action_settings&quot;&#xa;        android:orderInCategory=&quot;100&quot;&#xa;        android:title=&quot;@string/action_settings&quot;&#xa;        app:showAsAction=&quot;never&quot; /&gt;&#xa;&lt;/menu&gt;&#xa;"/>
+</node>
 </node>
 <node CREATED="1541656317768" ID="Freemind_Link_646209846" MODIFIED="1541656324633" POSITION="right" TEXT="&#x6ed1;&#x52a8;&#x83dc;&#x5355;" VSHIFT="79">
 <node CREATED="1541656326164" ID="Freemind_Link_692965439" MODIFIED="1541656332718" TEXT="DrawerLayout">
@@ -31,7 +34,7 @@
 <node CREATED="1541657268498" ID="Freemind_Link_786234583" MODIFIED="1541657301097" POSITION="right" TEXT="CardView &#x5b9e;&#x9645;&#x4e0a;&#x4e5f;&#x662f;&#x4e00;&#x4e2a; FrameLayout" VSHIFT="68">
 <node CREATED="1541658696405" ID="Freemind_Link_1772720083" MODIFIED="1541658698076" TEXT="&lt;android.support.v7.widget.CardView"/>
 </node>
-<node CREATED="1541657587606" ID="Freemind_Link_601607843" MODIFIED="1541657595585" POSITION="right" TEXT="AppBarLayout" VSHIFT="28">
+<node CREATED="1541657587606" ID="Freemind_Link_601607843" MODIFIED="1542767544103" POSITION="right" TEXT="AppBarLayout&#xa;&#x5b9e;&#x9645;&#x4e0a;&#x662f;&#x4e00;&#x4e2a;&#x5782;&#x76f4;&#x65b9;&#x5411;&#x4e0a;&#x7684;LineaLayout&#xff0c;&#xa;&#x4ed6;&#x5728;&#x5185;&#x90e8;&#x505a;&#x4e86;&#x5f88;&#x591a;&#x6eda;&#x52a8;&#x4e8b;&#x4ef6;&#x7684;&#x5c01;&#x88c5;" VSHIFT="28">
 <node CREATED="1541658419943" ID="Freemind_Link_165365067" MODIFIED="1541658421034" TEXT="&lt;android.support.design.widget.AppBarLayout"/>
 </node>
 <node CREATED="1541657747293" HGAP="21" ID="Freemind_Link_620562152" MODIFIED="1541657765881" POSITION="right" TEXT="&#x4e0b;&#x62c9;&#x5237;&#x65b0; SwipRefreshLayout" VSHIFT="30">
@@ -40,8 +43,38 @@
 <node CREATED="1541657875770" ID="Freemind_Link_1474012885" MODIFIED="1541657963880" POSITION="right" TEXT="&#x53ef;&#x6298;&#x53e0;&#x5f0f;&#x6807;&#x9898;&#x680f; CollapsingToolbarLayout&#xa;&#xa;CollapsingToolbarLayout &#x5fc5;&#x987b;&#x662f;AppBarLayout&#x7684;&#x5b50;&#x5e03;&#x5c40;&#xa;&#xa;AppBarLayout &#x5fc5;&#x987b;&#x662f; CoordinatoryLayout &#x7684;&#x5b50;&#x5e03;&#x5c40;&#xa;&#xa;" VSHIFT="50"/>
 <node CREATED="1541658072332" ID="Freemind_Link_1972560042" MODIFIED="1541658280673" POSITION="right" TEXT="NestedScrollView &#x6bd4; ScrollView &#x589e;&#x52a0;&#x4e86;&#x5d4c;&#x5957;&#x54cd;&#x5e94;&#x6eda;&#x52a8;&#x4e8b;&#x4ef6;&#x7684;&#x529f;&#x80fd;" VSHIFT="31"/>
 <node CREATED="1541658281859" ID="Freemind_Link_1144025614" MODIFIED="1541658315641" POSITION="right" TEXT="&#x5229;&#x7528;&#x7cfb;&#x7edf;&#x72b6;&#x6001;&#x680f;&#x7a7a;&#x95f4; android:fitsSystemWindows" VSHIFT="44"/>
-<node CREATED="1541656693820" ID="Freemind_Link_1075580565" MODIFIED="1541656722879" POSITION="left" TEXT="&#x7acb;&#x9762;&#x8bbe;&#x8ba1;&#xff1a;&#x754c;&#x9762;&#x4e0d;&#x662f;&#x4e00;&#x4e2a;&#x5e73;&#x9762;&#xff0c;&#x800c;&#x662f;&#x7acb;&#x4f53;&#x7684;&#x3002; &#x5982;&#x60ac;&#x6d6e;&#x6309;&#x94ae;&#x3002;"/>
-<node CREATED="1541657254704" HGAP="34" ID="Freemind_Link_386585323" MODIFIED="1541657265081" POSITION="left" TEXT="&#x5361;&#x7247;&#x5f0f;&#x5e03;&#x5c40;" VSHIFT="87"/>
-<node CREATED="1541657861596" ID="Freemind_Link_1933105491" MODIFIED="1541658557239" POSITION="left" TEXT="Design Support &#x5e93;&#xa;support-v4 &#x5e93;&#xa;appcompat-v7 &#x5e93;" VSHIFT="56"/>
+<node CREATED="1541656693820" ID="Freemind_Link_1075580565" MODIFIED="1542765722126" POSITION="left" TEXT="&#x7acb;&#x9762;&#x8bbe;&#x8ba1;&#xff1a;&#x754c;&#x9762;&#x4e0d;&#x662f;&#x4e00;&#x4e2a;&#x5e73;&#x9762;&#xff0c;&#x800c;&#x662f;&#x7acb;&#x4f53;&#x7684;&#x3002; &#x5982;&#x60ac;&#x6d6e;&#x6309;&#x94ae;&#x3002;&#xa;&#x5149;&#x7167;&#x548c;&#x6295;&#x5f71;,&#x8868;&#x660e;&#x5bf9;&#x8c61;&#x7a7a;&#x95f4;&#x8ddd;&#x79bb;, Z&#x8f74;&#x7684;&#x8ddd;&#x79bb;&#xa;&#x6709;&#x56fa;&#x5b9a;1dp&#x7684;&#x539a;&#x5ea6;"/>
+<node CREATED="1542766952182" HGAP="30" ID="Freemind_Link_281953523" MODIFIED="1542766987288" POSITION="left" TEXT="&#x53c2;&#x8003;&#x8d44;&#x6599;" VSHIFT="80">
+<node CREATED="1542766974035" ID="Freemind_Link_1945344039" MODIFIED="1542767312006" TEXT="&#x5982;&#x679c;&#x4f60;&#x4e0d;&#x719f;&#x6089;Material Design&#xff0c;&#x8bf7;&#x4e00;&#x53e3;&#x5403;&#x4e0b;&#x8fd9;&#x7bc7;&#x5e72;&#x8d27;&#xa;https://www.uisdc.com/material-design-knowledge&#xa;&#xa;&#x60f3;&#x77e5;&#x9053;&#x4ec0;&#x4e48;&#x662f; Material Design&#xff1f;&#x6709;&#x8fd9;&#x5341;&#x6b3e;&#x5e94;&#x7528;&#x5c31;&#x591f;&#x4e86; &#xa;https://www.aliyun.com/jiaocheng/72114.html&#xa;------&#xa;"/>
+</node>
+<node CREATED="1542767314713" ID="Freemind_Link_248730921" MODIFIED="1542767326995" POSITION="left" TEXT="&#x53c2;&#x8003;&#x8bbe;&#x8ba1;">
+<node CREATED="1542767327936" ID="Freemind_Link_605606438" MODIFIED="1542767396446" TEXT="Matrand&#xff0c;Phonograph Music Player &#xff0c;Google Calendar &#xa;Google+ &#xff0c;Bing Bong &#xff0c;Headspace &#xff0c;Evernote&#xa;Microsoft Health " VSHIFT="44"/>
+</node>
+<node CREATED="1541657254704" HGAP="34" ID="Freemind_Link_386585323" MODIFIED="1541657265081" POSITION="left" TEXT="&#x5361;&#x7247;&#x5f0f;&#x5e03;&#x5c40;" VSHIFT="87">
+<node CREATED="1542766129022" ID="Freemind_Link_857502094" MODIFIED="1542766968902" TEXT="&#x5bb9;&#x5668;&#xa;&#x7f29;&#x7565;&#x56fe;&#xa;&#x6807;&#x9898;&#x6587;&#x5b57;&#xa;&#x5c0f;&#x6807;&#x9898;&#xa;&#x591a;&#x5a92;&#x4f53;&#xa;&#x8f85;&#x52a9;&#x6587;&#x5b57;&#xa;&#x6309;&#x94ae;&#xa;&#x56fe;&#x6807;"/>
+</node>
+<node CREATED="1542765828465" HGAP="27" ID="Freemind_Link_1035960827" MODIFIED="1542765913373" POSITION="left" TEXT="&#x5e95;&#x90e8;&#x5e94;&#x7528;&#x680f;&#xa;App bars bottom" VSHIFT="18">
+<node CREATED="1542765858835" ID="Freemind_Link_710446295" MODIFIED="1542765885315" TEXT="&#x5bb9;&#x5668;&#xff0c;&#x5bfc;&#x822a;&#x62bd;&#x5c49;&#xff0c;&#x60ac;&#x6d6e;&#x6309;&#x94ae;&#xff0c;&#x52a8;&#x4f5c;&#x56fe;&#x6807;&#xff0c;&#x66f4;&#x591a;&#x83dc;&#x5355;&#x63a7;&#x4ef6;"/>
+</node>
+<node CREATED="1542765923557" ID="Freemind_Link_1163311153" MODIFIED="1542765940913" POSITION="left" TEXT="&#x9876;&#x90e8;&#x5e94;&#x7528;&#x680f;&#xa;App bars top"/>
+<node CREATED="1542766007664" ID="Freemind_Link_202898254" MODIFIED="1542766017530" POSITION="left" TEXT="&#x80cc;&#x677f;&#x8bbe;&#x8ba1; Backdrop"/>
+<node CREATED="1542766017957" ID="Freemind_Link_1593151382" MODIFIED="1542766039777" POSITION="left" TEXT="&#x6a2a;&#x5e45; Banner"/>
+<node CREATED="1542766058057" ID="Freemind_Link_176946015" MODIFIED="1542766068472" POSITION="left" TEXT="&#x5e95;&#x90e8;&#x5bfc;&#x822a; Bottom navigation"/>
+<node CREATED="1542766236784" ID="Freemind_Link_1207078208" MODIFIED="1542766243858" POSITION="left" TEXT="&#x7eb8;&#x7247; chips"/>
+<node CREATED="1542766289680" ID="Freemind_Link_1148967007" MODIFIED="1542766294428" POSITION="left" TEXT="&#x5bf9;&#x8bdd;&#x6846; Dialogs"/>
+<node CREATED="1542766294980" ID="Freemind_Link_1682720754" MODIFIED="1542766299713" POSITION="left" TEXT="&#x5206;&#x5272;&#x7ebf; divider"/>
+<node CREATED="1542766321928" ID="Freemind_Link_1133491454" MODIFIED="1542766335965" POSITION="left" TEXT="&#x62bd;&#x5c49;&#x5f0f;&#x5bfc;&#x822a; Navigation drawer">
+<node CREATED="1542766363497" ID="Freemind_Link_1303273785" MODIFIED="1542766412371" TEXT="&#x5bb9;&#x5668;&#xff0c;&#x5934;&#x90e8;&#xff0c;&#x5206;&#x5272;&#x7ebf;&#xff0c;&#x9009;&#x4e2d;&#x6001;&#xff0c;&#x9009;&#x4e2d;&#x6587;&#x672c;&#xff0c;&#x6ca1;&#x6709;&#x6fc0;&#x6d3b;&#x7684;&#x6587;&#x672c;&#xff0c;&#x5c0f;&#x6807;&#x9898;&#xff0c;&#x5e95;&#x5c42;&#x754c;&#x9762;&#xff08;&#x4e0d;&#x53ef;&#x64cd;&#x4f5c;&#xff09;"/>
+</node>
+<node CREATED="1542766423898" ID="Freemind_Link_1687966217" MODIFIED="1542766436547" POSITION="left" TEXT="&#x9875;&#x5361; Tabs"/>
+<node CREATED="1542766452456" ID="Freemind_Link_1829082620" MODIFIED="1542766466677" POSITION="left" TEXT="&#x6587;&#x5b57;&#x8f93;&#x5165;&#x6846; Text fields"/>
+<node CREATED="1542766480574" ID="Freemind_Link_1972963537" MODIFIED="1542766519698" POSITION="left" TEXT="&#x56fe;&#x7247;&#x7ec4; Image Lists" VSHIFT="55">
+<node CREATED="1542766520796" ID="Freemind_Link_1785701814" MODIFIED="1542766668125" TEXT="&#x6b63;&#x5e38;&#x56fe;&#x7247;-&#x6bcf;&#x5f20;&#x56fe;&#x7247;&#x5927;&#x5c0f;&#x4e00;&#x6837;&#xa;&#x6392;&#x7248;&#x56fe;&#x7247;-&#x6709;&#x9192;&#x76ee;&#x56fe;&#x7247;&#x662f;&#x5176;&#x5b83;&#x666e;&#x901a;&#x56fe;&#x7247;&#x7684;4&#x500d;&#xff0c;&#x5bbd;&#x5c3a;&#x5bf8;&#x56fe;&#x7247;&#xa;&#x7167;&#x7247;&#x5899;-&#xa;&#x7011;&#x5e03;&#x6d41;-&#x5bbd;&#x5ea6;&#x4e00;&#x6837;&#xff0c;&#x9ad8;&#x5ea6;&#x4e0d;&#x540c;&#x7684;&#x56fe;&#x7247;&#x6392;&#x5217;"/>
+</node>
+<node CREATED="1542766671814" ID="Freemind_Link_68637382" MODIFIED="1542766677295" POSITION="left" TEXT="&#x6ed1;&#x5757; Sliders"/>
+<node CREATED="1542766699608" HGAP="21" ID="Freemind_Link_997125329" MODIFIED="1542766713658" POSITION="left" TEXT="&#x9009;&#x62e9;&#x5668; Selection controls" VSHIFT="18"/>
+<node CREATED="1542766726482" HGAP="26" ID="Freemind_Link_1576527192" MODIFIED="1542766737351" POSITION="left" TEXT="&#x5e95;&#x90e8;&#x63d0;&#x793a;&#x680f; Snackbars" VSHIFT="36"/>
+<node CREATED="1542766754591" HGAP="25" ID="Freemind_Link_1522660418" MODIFIED="1542766770079" POSITION="left" TEXT="&#x72b6;&#x6001;&#x6307;&#x5f15; Progress indicators" VSHIFT="34"/>
+<node CREATED="1541657861596" HGAP="21" ID="Freemind_Link_1933105491" MODIFIED="1542766234358" POSITION="left" TEXT="Design Support &#x5e93;&#xa;support-v4 &#x5e93;&#xa;appcompat-v7 &#x5e93;" VSHIFT="165"/>
 </node>
 </map>
